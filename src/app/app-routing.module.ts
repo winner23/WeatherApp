9b2115a2c638b93c4c84d32d57a8d560bf447bb0
Volodymyr Vinyarskyy <v.winner@gmail.com>
2019-01-14 09:09:12 +0200

    Initial commit: added bootstrap, defined routing for future components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './shell/home/home.component';
+import { ThreeForecastComponent } from './shell/three-forecast/three-forecast.component';
+import { TenForecastComponent } from './shell/ten-forecast/ten-forecast.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: HomeComponent
+  },
+  {
+    path: 'forecast3',
+    component: ThreeForecastComponent
+  },
+  {
+    path: 'forecast10',
+    component: TenForecastComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormComponent } from './shell/form/form.component';
+import { HomeComponent } from './shell/home/home.component';
+import { ThreeForecastComponent } from './shell/three-forecast/three-forecast.component';
+import { TenForecastComponent } from './shell/ten-forecast/ten-forecast.component';
+
 
 @NgModule({
   declarations: [
+    FormComponent,
+    HomeComponent,
+    ThreeForecastComponent,
+    TenForecastComponent,
     AppComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
