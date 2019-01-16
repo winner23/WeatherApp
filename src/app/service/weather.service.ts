3e9a325f8f12efb61c1a0fc0ab06d7e638557ad0
Volodymyr Vinyarskyy <v.winner@gmail.com>
2019-01-16 23:50:11 +0200

    Improve weather service for getting forecast

--- src/app/service/weather.service.ts
+++ src/app/service/weather.service.ts
@@ -3,26 +3,28 @@ import { HttpClient } from "@angular/common/http";
 import { Observable, throwError } from 'rxjs';
 import { map, catchError } from 'rxjs/operators';
 import { Current } from '../models/current.model';
+import { Forecast } from '../models/forecast.model';
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
   private API_KEY = "dda0df6af6afa64c2dec9d82fb70f2a7";
   public cityName: string;
-  private API_URL = 'https://api.openweathermap.org/data/2.5/weather?';
+  private API_URL = 'https://api.openweathermap.org/data/2.5/';
   public currentWeather: Current;
+  
+  public forecastDayky: Forecast[];
   error: string;
   units = "metric";
   constructor(private http: HttpClient) { }
 
-  get url() {
+  url(type: string) {
     if (this.cityName) {
       const requestUrl = `q=${this.cityName}&appid=${this.API_KEY}&units=${this.units || "metric"}`;
-      return this.API_URL + requestUrl;
+      return this.API_URL + type + "?" + requestUrl;
     }
     return; 
   }
-
   unitFor(data: string): string {
     switch (data) {
       case "temp":
@@ -33,9 +35,9 @@ export class WeatherService {
         break;
     }
   }
-  get data(): Observable<Current> {
+  get weather(): Observable<Current> {
     if (this.cityName && this.cityName.length > 2) {
-    return this.http.get<Current>(this.url)
+    return this.http.get<Current>(this.url("weather"))
     .pipe(map( (response: any) => {
       this.error = undefined;
       return response;
@@ -48,6 +50,32 @@ export class WeatherService {
       this.currentWeather = undefined;
       return;
     }
+  }
+  forecast(hourly = true): Observable<Forecast[]> {
+    if (hourly && this.currentWeather) {
+      return this.http.get<Forecast[]>(this.url("forecast"))
+      .pipe(map( (response: any) => {
+        this.error = undefined;
+        return response.list;
+      }), catchError( (error: any) => {
+        this.currentWeather = undefined;
+        this.error = error.statusText;
+        return throwError(error);
+      }));
+      } else if (this.currentWeather) {
+        return this.http.get<Forecast[]>(this.url("forecast")+"&cnt=10")
+      .pipe(map( (response: any) => {
+        this.error = undefined;
+        return response.list;
+      }), catchError( (error: any) => {
+        this.currentWeather = undefined;
+        this.error = error.statusText;
+        return throwError(error);
+      }));
+      } else {
+        this.currentWeather = undefined;
+        return;
+      }
   }
 
 }
