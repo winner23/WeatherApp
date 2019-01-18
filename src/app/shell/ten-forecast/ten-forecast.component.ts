3e0757e33660140d8eb6edfd9f87b01a0cc657c0
Volodymyr Viniarskyi <vvini@softserveinc.com>
2019-01-18 17:05:47 +0200

    Added 10 day forecast

--- src/app/shell/ten-forecast/ten-forecast.component.ts
+++ src/app/shell/ten-forecast/ten-forecast.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { WeatherService } from 'src/app/service/weather.service';
-import { Forecast } from 'src/app/models/forecast.model';
-
+import { ForecastDaily } from 'src/app/models/forecast-daily.model';
 @Component({
   selector: 'app-ten-forecast',
   templateUrl: './ten-forecast.component.html',
@@ -9,11 +8,41 @@ import { Forecast } from 'src/app/models/forecast.model';
 })
 export class TenForecastComponent implements OnInit {
 
-  public forecastHourly: Forecast[];
+  public forecastDaily: ForecastDaily[];
 
   constructor(public serviceWeather: WeatherService) { }
 
   ngOnInit() {
+    this.getData();
   }
-
+  getData() {
+    if (this.serviceWeather.currentWeather && !this.serviceWeather.error) {
+      try {
+        this.serviceWeather.forecast(false).subscribe( (forecast: any) => {
+          if (forecast && forecast.length > 0) {
+            this.forecastDaily = forecast;
+          }
+        })
+      } catch (error) {
+        this.forecastDaily = undefined;
+        this.serviceWeather.error = error;
+      }
+    }
+  }
+  date(date: number): string {
+    const certernDate = new Date();
+    certernDate.setTime(date);
+    const converted = certernDate.toDateString();
+    return converted;
+  }
+  weatherImgUrl(listItem: ForecastDaily): string {
+    if (listItem && listItem.weather.length > 0) {
+      const weather = listItem.weather[0];
+      if (weather) {
+        const resultUrl = `https://openweathermap.org/img/w/${weather.icon}.png`;
+        return resultUrl;
+      }
+    }
+    return;
+  }
 }
